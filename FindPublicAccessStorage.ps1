7d5e15270b93ab34fad4666ee25fad8fd15fe73d
Mike Hsu <mikeehsu@gmail.com>
2018-01-10 10:12:25 -0500

    updated documentation & added login detection

--- FindPublicAccessStorage.ps1
+++ FindPublicAccessStorage.ps1
@@ -1,3 +1,32 @@
+##############################
+#.SYNOPSIS
+# Find publicly accessible storage containers
+#
+#.DESCRIPTION
+# This script will loop through all containers and output
+# the names of any where PublicAccess is not set to "Off"
+##
+#.EXAMPLE
+# FindPublicAccessStorage.ps1
+#
+#.NOTES
+#
+##############################
+
+# confirm user is logged into subscription
+try {
+    $result = Get-AzureRmContext -ErrorAction Stop
+    if (-not $result.Environment) {
+        Write-Error "Please login (Login-AzureRmAccount) and set the proper subscription (Select-AzureRmSubscription) context before proceeding."
+        exit
+    }
+    $azureEnvironmentName = $result.Environment.Name
+
+} catch {
+    Write-Error "Please login (Login-AzureRmAccount) and set the proper subscription (Select-AzureRmSubscription) context before proceeding."
+    exit
+}
+
 $storageAccounts = Get-AzureRMStorageAccount
 
 foreach ($storageAccount in $storageAccounts) {
@@ -15,6 +44,6 @@ foreach ($storageAccount in $storageAccounts) {
         Where-Object {$_.PublicAccess -ne 'Off' }
 
     foreach ($container in $publicOnContainers) {
-        Write-Output "WARNING!! $($storageAccount.StorageAccountName) > $($container.Name) is publicly accessible"
+        Write-Output "WARNING!! $($storageAccount.StorageAccountName)/$($container.Name) is publicly accessible"
     }
 }
