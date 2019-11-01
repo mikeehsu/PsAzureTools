f5130022e0f708f2af3f721b58a60f807446fb90
Mike Hsu <mikeehsu@gmail.com>
2019-11-01 09:15:10 -0400

    package all backups into zip file

--- BackupKeyVault.ps1
+++ BackupKeyVault.ps1
@@ -1,9 +1,10 @@
 Param (
     [Parameter(Mandatory = $true)]
-    [string] $VaultName
-)
+    [string] $VaultName,
 
-$backupPath = "./$VaultName"
+    [Parameter(Mandatory = $false)]
+    [string] $OutputFile
+)
 
 #################################################
 # MAIN
@@ -19,35 +20,63 @@ catch {
     throw "Please login (Connect-AzAccount) and set the proper subscription context before proceeding."
 }
 
-$vault = Get-AzKeyVault -VaultName $VaultName -ErrorAction Stop
-if (-not $vault) {
-    throw "Unable to open key vault - $VaultName"
-    return
+# check OutputFile path
+if (-not $OutputFile) {
+    $OutputFile = "$VaultName-backup.zip"
 }
 
+if (Test-Path $OutputFile) {
+    throw "$OutputFIle already exsists. Please remove before starting backup."
+}
+
+# create folder for storing keys
+$now = Get-Date
+if ($env:TEMP) {
+    $backupPath = "$env:TEMP/$VaultName" + $now.ToString("yyyyMMddhhmm")
+}
+else {
+    $backupPath = "./$VaultName" + $now.ToString("yyyyMMddhhmm")
+}
 
 if (Test-Path "$backupPath/*") {
     throw "Backup folder $backupPath exists. Please make sure it is empty before starting."
 }
 $null = New-Item -Path $backupPath -ItemType Directory -ErrorAction Stop
 
+
+# get keyvault
+$vault = Get-AzKeyVault -VaultName $VaultName -ErrorAction Stop
+if (-not $vault) {
+    throw "Unable to open key vault - $VaultName"
+    return
+}
+
 $count = 0
 $keys = Get-AzKeyVaultKey -VaultName $vault.VaultName
 foreach ($key in $keys) {
-    Backup-AzKeyVaultKey -VaultName $VaultName -Name $key.Name -OutputFile "$backupPath/$($key.Name).key"
+    $null = Backup-AzKeyVaultKey -VaultName $VaultName -Name $key.Name -OutputFile "$backupPath/$($key.Name).key"
+    Write-Verbose $key.Name
     $count += 1
 }
 
 $secrets = Get-AzKeyVaultSecret -VaultName $vault.VaultName
 foreach ($secret in $secrets) {
-    Backup-AzKeyVaultSecret -VaultName $VaultName -Name $secret.Name -OutputFile "$backupPath/$($secret.Name).secret"
+    $null = Backup-AzKeyVaultSecret -VaultName $VaultName -Name $secret.Name -OutputFile "$backupPath/$($secret.Name).secret"
+    Write-Verbose $secret.Name
     $count += 1
 }
 
-$certificates = Get-AzKeyVaultCertificate -VaultName $vault.VaultName
-foreach ($certificate in $certificates) {
-    Backup-AzKeyVaultCertificate -VaultName $VaultName -Name $certificate.Name -OutputFile "$backupPath/$($certificate.Name).certificate"
+$certs = Get-AzKeyVaultCertificate -VaultName $vault.VaultName
+foreach ($cert in $certs) {
+    $null = Backup-AzKeyVaultCertificate -VaultName $VaultName -Name $cert.Name -OutputFile "$backupPath/$($cert.Name).certificate"
+    Write-Verbose $cert.name
     $count += 1
 }
 
-Write-Output "$count keys/secrets/certificates backed up"
+# package all the backup files into a zip
+Compress-Archive -Path "$backupPath/*" -DestinationPath $OutputFile
+
+# clean up
+Remove-item -Path $backupPath -Force -Recurse
+
+Write-Output "$count items backed up to $OutputFile"
