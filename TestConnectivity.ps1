ad0599513e32dd488785ca6e7631fcba24d5e89e
Mike Hsu <mikeehsu@gmail.com>
2020-04-17 12:42:54 -0400

    added ability to provie CIDR range

--- TestConnectivity.ps1
+++ TestConnectivity.ps1
@@ -43,31 +43,33 @@ This parameter sets the waiting period for connections before the attempt is con
 [CmdletBinding()]
 
 Param(
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
+    [Alias("IpAddress")]
     [string[]] $IpAddresses,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
+    [Alias("Port")]
     [int[]] $Ports,
 
-    [Parameter(Mandatory=$false)]
-    [switch] $Details,
-
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [string] $FilePath,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
+    [switch] $Details,
+
+    [Parameter(Mandatory = $false)]
     [string] $OutputPath,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [switch] $Continuous,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [switch] $OnlyShowFailed,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [int] $Frequency = 5,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [int] $TimeOut = 500
 )
 
@@ -84,15 +86,191 @@ Class TestCase {
     [string] $Status
 }
 
+
+#####################################################################
+Function Get-IPv4NetworkInfo {
+    <#
+    .SYNOPSIS
+    Gets extended information about an IPv4 network.
+
+    .DESCRIPTION
+    Gets Network Address, Broadcast Address, Wildcard Mask. and usable host range for a network given the IP address and Subnet Mask.
+
+    .PARAMETER IPAddress
+    IP Address of any ip within the network Note: Exclusive from @CIDRAddress
+
+    .PARAMETER SubnetMask
+    Subnet Mask of the network. Note: Exclusive from @CIDRAddress
+
+    .PARAMETER CIDRAddress
+    CIDR Notation of IP/Subnet Mask (x.x.x.x/y) Note: Exclusive from @IPAddress and @SubnetMask
+
+    .PARAMETER IncludeIPRange
+    Switch parameter that defines whether or not the script will return an array of usable host IP addresses within the defined network.
+    Note: This parameter can cause delays in script completion for larger subnets.
+
+    .EXAMPLE
+    Get-IPv4NetworkInfo -IPAddress 192.168.1.23 -SubnetMask 255.255.255.0
+
+    Get network information with IP Address and Subnet Mask
+
+    .EXAMPLE
+    Get-IPv4NetworkInfo -CIDRAddress 192.168.1.23/24
+
+    Get network information with CIDR Notation
+
+    .NOTES
+    File Name  : Get-IPv4NetworkInfo.ps1
+    Author     : Ryan Drane
+    Date       : 5/10/16
+    Requires   : PowerShell v3
+
+    .LINK
+    https://www.ryandrane.com
+    https://www.ryandrane.com/2016/05/getting-ip-network-information-powershell/
+    #>
+
+    Param
+    (
+        [Parameter(ParameterSetName = "IPandMask", Mandatory = $true)]
+        [ValidateScript( { $_ -match [ipaddress]$_ })]
+        [System.String] $IPAddress,
+
+        [Parameter(ParameterSetName = "IPandMask", Mandatory = $true)]
+        [ValidateScript( { $_ -match [ipaddress]$_ })]
+        [System.String] $SubnetMask,
+
+        [Parameter(ParameterSetName = "CIDR", Mandatory = $true)]
+        [ValidateScript( { $_ -match '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/([0-9]|[0-2][0-9]|3[0-2])$' })]
+        [System.String] $CIDRAddress,
+
+        [Switch] $IncludeIPRange
+    )
+
+    # If @CIDRAddress is set
+    if ($CIDRAddress) {
+        # Separate our IP address, from subnet bit count
+        $IPAddress, [int32]$MaskBits = $CIDRAddress.Split('/')
+
+        # Create array to hold our output mask
+        $CIDRMask = @()
+
+        # For loop to run through each octet,
+        for ($j = 0; $j -lt 4; $j++) {
+            # If there are 8 or more bits left
+            if ($MaskBits -gt 7) {
+                # Add 255 to mask array, and subtract 8 bits
+                $CIDRMask += [byte]255
+                $MaskBits -= 8
+            }
+            else {
+                # bits are less than 8, calculate octet bits and
+                # zero out our $MaskBits variable.
+                $CIDRMask += [byte]255 -shl (8 - $MaskBits)
+                $MaskBits = 0
+            }
+        }
+
+        # Assign our newly created mask to the SubnetMask variable
+        $SubnetMask = $CIDRMask -join '.'
+    }
+
+    # Get Arrays of [Byte] objects, one for each octet in our IP and Mask
+    $IPAddressBytes = ([ipaddress]::Parse($IPAddress)).GetAddressBytes()
+    $SubnetMaskBytes = ([ipaddress]::Parse($SubnetMask)).GetAddressBytes()
+
+    # Declare empty arrays to hold output
+    $NetworkAddressBytes = @()
+    $BroadcastAddressBytes = @()
+    $WildcardMaskBytes = @()
+
+    # Determine Broadcast / Network Addresses, as well as Wildcard Mask
+    for ($i = 0; $i -lt 4; $i++) {
+        # Compare each Octet in the host IP to the Mask using bitwise
+        # to obtain our Network Address
+        $NetworkAddressBytes += $IPAddressBytes[$i] -band $SubnetMaskBytes[$i]
+
+        # Compare each Octet in the subnet mask to 255 to get our wildcard mask
+        $WildcardMaskBytes += $SubnetMaskBytes[$i] -bxor 255
+
+        # Compare each octet in network address to wildcard mask to get broadcast.
+        $BroadcastAddressBytes += $NetworkAddressBytes[$i] -bxor $WildcardMaskBytes[$i]
+    }
+
+    # Create variables to hold our NetworkAddress, WildcardMask, BroadcastAddress
+    $NetworkAddress = $NetworkAddressBytes -join '.'
+    $BroadcastAddress = $BroadcastAddressBytes -join '.'
+    $WildcardMask = $WildcardMaskBytes -join '.'
+
+    # Now that we have our Network, Widcard, and broadcast information,
+    # We need to reverse the byte order in our Network and Broadcast addresses
+    [array]::Reverse($NetworkAddressBytes)
+    [array]::Reverse($BroadcastAddressBytes)
+
+    # We also need to reverse the array of our IP address in order to get its
+    # integer representation
+    [array]::Reverse($IPAddressBytes)
+
+    # Next we convert them both to 32-bit integers
+    $NetworkAddressInt = [System.BitConverter]::ToUInt32($NetworkAddressBytes, 0)
+    $BroadcastAddressInt = [System.BitConverter]::ToUInt32($BroadcastAddressBytes, 0)
+    # $IPAddressInt        = [System.BitConverter]::ToUInt32($IPAddressBytes,0)
+
+    #Calculate the number of hosts in our subnet, subtracting one to account for network address.
+    $NumberOfHosts = ($BroadcastAddressInt - $NetworkAddressInt) - 1        
+
+    #Calculate the max and min usable host IPs.
+    if ($NumberOfHosts -gt 1) {
+        $HostMinIP = [ipaddress]([convert]::ToDouble($NetworkAddressInt + 1)) | Select-Object -ExpandProperty IPAddressToString
+        $HostMaxIP = [ipaddress]([convert]::ToDouble($NetworkAddressInt + $NumberOfHosts)) | Select-Object -ExpandProperty IPAddressToString
+
+        # Declare an empty array to hold our range of usable IPs.
+        $IPRange = @()
+
+        # If -IncludeIPRange specified, calculate it
+        if ($IncludeIPRange) {
+            # Now run through our IP range and figure out the IP address for each.
+            For ($j = 1; $j -le $NumberOfHosts; $j++) {
+                # Increment Network Address by our counter variable, then convert back
+                # lto an IP address and extract as string, add to IPRange output array.
+                $IPRange += [ipaddress]([convert]::ToDouble($NetworkAddressInt + $j)) | Select-Object -ExpandProperty IPAddressToString
+            }
+        }
+    }
+    else {
+        # brokend out to accommodate /32 blocks
+        $HostMinIP = $IPAddress
+        $HostMaxIP = $IPAddress
+        $IpRange = @($IPAddress)
+    }
+
+    # Create our output object
+    $obj = New-Object -TypeName psobject
+
+    # Add our properties to it
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "IPAddress"         -Value $IPAddress
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "SubnetMask"        -Value $SubnetMask
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "NetworkAddress"    -Value $NetworkAddress
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "BroadcastAddress"  -Value $BroadcastAddress
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "WildcardMask"      -Value $WildcardMask
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "NumberOfHostIPs"   -Value $NumberOfHosts
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "HostMinIp"         -Value $HostMinIP
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "HostMaxIp"         -Value $HostMaxIP
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "IPRange"           -Value $IPRange
+
+    # Return the object
+    return $obj
+}
+
 #####################################################################
 function GetIpInRange {
 
     Param(
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         [ValidatePattern("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")]
         [string] $StartIpAddress,
 
-        [parameter(Mandatory=$false)]
+        [parameter(Mandatory = $false)]
         [ValidatePattern("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")]
         [string] $EndIpAddress = $StartIPAddress
     )
@@ -123,26 +301,30 @@ function GetIpInRange {
         foreach ($b in ($startOctet2..$endOctet2)) {
             if ($b -eq $startOctet2) {
                 $startOctet3 = $startOctets[2]
-            } else {
+            }
+            else {
                 $startOctet3 = 0
             }
 
             if ($b -eq $endOctet2) {
                 $endOctet3 = $endOctets[2]
-            } else {
+            }
+            else {
                 $endOctet3 = 255
             }
 
             foreach ($c in ($startOctet3..$endOctet3)) {
                 if ($b -eq $startOctet2 -and $c -eq $startOctet3) {
                     $startOctet4 = $startOctets[3]
-                } else {
+                }
+                else {
                     $startOctet4 = 0
                 }
 
                 if ($b -eq $endOctet2 -and $c -eq $endOctet3) {
                     $endOctet4 = $endOctets[3]
-                } else {
+                }
+                else {
                     $endOctet4 = 255
                 }
 
@@ -156,15 +338,48 @@ function GetIpInRange {
     return $ipSet
 }
 
+############################################################################
+Function ParseIpAddressRange {
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $IpAddressRange
+    )
+ 
+    if ($IpAddressRange -like '*-*') {
+        $ipRange = $IpAddressRange.Split('-')
+
+        $startIp = $ipRange[0]
+        if ($ipRange.Length -eq 1) {
+            $endIp = $startIp
+        }
+        else {
+            $endIp = $ipRange[1]
+        }
+        $ipSet = GetIpInRange -StartIpAddress $startIp -EndIpAddress $endIp
+    }
+    elseif ($IpAddressRange -like '*/*') {
+        $ipSet = (Get-IPv4NetworkInfo -CIDRAddress $IpAddressRange -IncludeIPRange).IPRange
+
+    }
+    else {
+        $ipSet = $test.IpAddress.Split(';')
+    }
+
+    return $ipSet
+
+}
 
 ############################################################################
+
+Set-StrictMode -Version 3
 
 $report = @()
 $testCases = @()
 
 # validate parameters
 if (-not $Ports) {
-    $Ports = @(80)
+    $Ports = @(80,443)
 }
 
 if (-not ($FilePath -or $IpAddresses)) {
@@ -177,22 +392,10 @@ if ($OutputPath -and $Continuous) {
     return
 }
 
-
 # setup test cases
 if ($IpAddresses) {
     foreach ($IpAddress in $IpAddresses) {
-        if ($ipAddress -like '*-*') {
-            $ipRange = $IpAddress.Split('-')
-            $startIp = $ipRange[0]
-            if ($ipRange.Length -eq 1) {
-                $endIp = $startIp
-            } else {
-                $endIp = $ipRange[1]
-            }
-            $ipSet = GetIpInRange -StartIpAddress $startIp -EndIpAddress $endIp
-        } else {
-            $ipSet = @($ipAddress)
-        }
+        $ipSet = ParseIpAddressRange -IpAddressRange $IpAddress
 
         foreach ($ip in $ipSet) {
             $testCase = [TestCase]::New()
@@ -212,11 +415,11 @@ if ($FilePath) {
 
     $testFile = Import-Csv -Path $FilePath -Delimiter ','
 
-    if ($testFile | Get-member -MemberType 'NoteProperty' | Where-Object {$_.Name -eq 'PortsToTest'}) {
+    if ($testFile | Get-member -MemberType 'NoteProperty' | Where-Object { $_.Name -eq 'PortsToTest' }) {
         $portsToTestIncluded = $true
     }
 
-    if ($testFile | Get-member -MemberType 'NoteProperty' | Where-Object {$_.Name -eq 'PortsExpected'}) {
+    if ($testFile | Get-member -MemberType 'NoteProperty' | Where-Object { $_.Name -eq 'PortsExpected' }) {
         $portsExpectedIncluded = $true
     }
 
@@ -226,7 +429,8 @@ if ($FilePath) {
         $portsToTest = @()
         if ($portsToTestIncluded -and ($test.PortsToTest.Trim().Length -gt 0)) {
             $portsToTest = $test.PortsToTest.Split(';')
-        } else {
+        }
+        else {
             $portsToTest = $Ports
         }
 
@@ -235,25 +439,13 @@ if ($FilePath) {
             if ($test.PortsExpected.Trim().Length -gt 0) {
                 $portsExpected = $test.PortsExpected.Split(';')
             }
-        } else {
+        }
+        else {
             $portsExpected = $portsToTest
         }
 
         # ipAddresses
-        if ($test.IpAddress -like '*-*') {
-            $ipRange = $test.IpAddress.Split('-')
-
-            $startIp = $ipRange[0]
-            if ($ipRange.Length -eq 1) {
-                $endIp = $startIp
-            } else {
-                $endIp = $ipRange[1]
-            }
-            $ipSet = GetIpInRange -StartIpAddress $startIp -EndIpAddress $endIp
-
-        } else {
-            $ipSet = $test.IpAddress.Split(';')
-        }
+        $ipSet = ParseIpAddressRange -IpAddressRange $test.IpAddress
 
         foreach ($ip in $ipSet) {
             $testCase = [TestCase]::New()
@@ -267,7 +459,7 @@ if ($FilePath) {
 }
 
 # do continuously
-do  {
+do {
 
     # execute the tests
     foreach ($test in $testCases) {
@@ -301,7 +493,8 @@ do  {
 
         if ($test.PortsFailed) {
             $test.Status = 'FAILED'
-        } else {
+        }
+        else {
             $test.Status = 'OK'
         }
     }
@@ -315,13 +508,14 @@ do  {
 
     # display results
     if ($Details) {
-        $report = $testCases | Select-Object -Property Status,IpAddress,PortsFailed,PortsExpected,PortsOpen
-    } else {
-        $report = $testCases | Select-Object -Property Status,IpAddress,PortsFailed
+        $report = $testCases | Select-Object -Property Status, IpAddress, PortsFailed, PortsExpected, PortsOpen
+    }
+    else {
+        $report = $testCases | Select-Object -Property Status, IpAddress, PortsFailed
     }
 
     if ($OnlyShowFailed) {
-        $report = $report | Where-Object {$_.Status -eq 'FAILED'}
+        $report = $report | Where-Object { $_.Status -eq 'FAILED' }
     }
 
     # display on screen
@@ -336,7 +530,7 @@ do  {
         Start-Sleep $Frequency
     }
 
-} While ($Continuous)
+} while ($Continuous)
 
 if ($OutputPath) {
     $report | Export-Csv $OutputPath
