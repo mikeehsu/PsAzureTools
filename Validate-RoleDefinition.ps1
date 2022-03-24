16afb54166491de3dfdbf661d66792b155895fcb
Mike Hsu <mikeehsu@gmail.com>
2022-03-24 13:22:49 -0400

    retool to validate multiple roles with low overhead

--- Validate-RoleDefinition.ps1
+++ Validate-RoleDefinition.ps1
@@ -3,68 +3,108 @@
 Validate Role Deinition permissions
 
 .DESCRIPTION
-Validate all the Service Provider Operations in a Role Definition and return only valid operations
-
-.PARAMETER FilePath
-Source file for the Role Definition
+Validate all the Provider Operations in a Role Definition and return a role with only the valid operations
 
 .PARAMETER Role
 Role Definition to validate
 
 .EXAMPLE
-Validate-RoleDefinition -FilePath ".\RoleDefinition.json"
+Get-AzRoleDefinition -Role <role> | Validate-RoleDefinition.ps1
 
-.EXAMPLE
-$role = $rolw | Validate-RoleDefinition.ps1
 #>
 
 [CmdletBinding()]
 param (
-    [Parameter(ParameterSetName='FilePath', Mandatory)]
-    [string] $FilePath,
-
-    [Parameter(ParameterSetName='Role', Mandatory, ValueFromPipeline)]
+    [Parameter(Mandatory, ValueFromPipeline)]
     [psobject] $Role
 )
 
-begin {
+BEGIN {
     # check for Azure context
     if (-not $(Get-AzContext -ErrorAction Stop)) {
         Write-Error 'Please login using Connect-AzAccount and try again.' -ErrorAction Stop
     }
 
-    if ($PSBoundParameters.ContainsKey('FilePath')) {
-        $Role = Get-Content $FilePath | ConvertFrom-Json
-    }
+    # load all Provider Operations into memory
+    Write-Progress 'Reading Provider Operations...'
+    $script:ExistingOperations = [System.Collections.ArrayList]::new()
+    Get-AzProviderOperation -ErrorAction Stop | ForEach-Object { $ExistingOperations += $_.Operation }
+    Write-Progress -Activity 'Reading Provider Operations...' -Completed
 }
 
-process {
-    Write-Verbose "checking provider opertations in $($Role.Name)"
+PROCESS {
 
-    $actions = [System.Collections.ArrayList]::new()
-    foreach ($roleAction in $Role.Actions) {
-        if (Get-AzProviderOperation -OperationSearchString $roleAction -ErrorAction Continue) {
-            $actions = $actions + $roleAction
-        } else {
-            Write-Warning "Provider operation '$roleAction' not found."
+    function GetValidActions
+    {
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [array] $Actions
+        )
+
+        $validActions = [System.Collections.ArrayList]::new()
+
+        foreach ($action in $Actions) {
+            if (-not $action) {
+                continue
+            }
+
+            $found = $false
+
+            # search for wildcard matches
+            foreach ($operation in $ExistingOperations) {
+                if ($operation -like $action) {
+                    $validActions += $action
+                    $found = $true
+                    break
+                }
+            }
+
+            # double check any items with /*/ in the action
+            # items like 'Microsoft.Storage/storageAccounts/*/delete' should match 'Microsoft.Storage/storageAccounts/delete'
+            if (-not $found -and $action -match '/*/') {
+                $actionMinusWildcard = $action.Replace('/*/', '/')
+                foreach ($operation in $ExistingOperations) {
+                    if ($operation -like $actionMinusWildcard) {
+                        $validActions += $action
+                        $found = $true
+                        break
+                    }
+                }
+            }
+
+            if (-not $found) {
+                Write-Warning "'$action' action  not found"
+            }
         }
+
+        return $validActions
     }
 
-    $notActions = [System.Collections.ArrayList]::new()
-    foreach ($roleNotAction in $Role.NotActions) {
-        if (Get-AzProviderOperation -OperationSearchString $roleNotAction -ErrorAction Continue) {
-            $notActions = $notActions + $roleNotAction
-        } else {
-            Write-Warning "Provider operation '$roleNotAction' not found"
-        }
+    # get validated operations
+    Write-Verbose "checking role...$($Role.Name)"
+    $actions = GetValidActions($Role.Actions)
+    $notActions = GetValidActions($Role.NotActions)
+
+    # check for differences from the original
+    $actionDiff = $null
+    if ($actions) {
+        $actionDiff = Compare-Object $Role.Actions $actions
     }
 
-    $newRole = $Role.PSObject.Copy()
-    $newRole.Actions = $actions
-    $newRole.NotActions = $notActions
-    return $newRole
+    $notActionDiff = $null
+    if ($notActions) {
+        $notActionDiff = Compare-Object $Role.NotActions $notActions
+    }
+
+    if ($actionDiff -or $notActionDiff) {
+        # differences found, output new role
+        $newRole = $Role.PSObject.Copy()
+        $newRole.Actions = $actions
+        $newRole.NotActions = $notActions
+        return $newRole
+    }
 }
 
-end {
+END {
 }
-
