849104b45314bda6bbe5be8314a99f8e24a73b03
Mike Hsu <mikeehsu@gmail.com>
2024-03-13 10:54:09 -0400

    support for multiple subscriptions

--- Write-UsageDetailToBlob.ps1
+++ Write-UsageDetailToBlob.ps1
@@ -20,6 +20,12 @@ Name of the Storage Account to write the CSV data to.
 .PARAMETER ContainerName
 Name of the Container to write the CSV data to.
 
+.PARAMETER $Subscription
+Name of a single or list of SubscriptionName, SubscriptionId to run the cost export for.
+
+.PARAMETER $AllSubscriptions
+Use this parameter to process all subscription in context. If neither, subscription or -AllSubscriptions only the current subscription will be exported
+
 .EXAMPLE
 .\Write-UsageDetailToBlob.ps1 -StartDate '2020-01-01' -EndDate '2020-01-31' -ResourceGroupName 'myResourceGroup' -StorageAccountName 'myStorageAccount' -ContainerName 'billing'
 
@@ -39,7 +45,13 @@ param (
     [string] $StorageAccountName,
 
     [Parameter()]
-    [string] $ContainerName
+    [string] $ContainerName,
+
+    [Parameter()]
+    [string[]] $Subscription,
+
+    [Parameter()]
+    [switch] $AllSubscriptions
 )
 
 Set-StrictMode -Version 2
@@ -68,7 +80,7 @@ if (-not $ContainerName) {
 }
 
 if (-not $ResourceGroupName -or -not $StorageAccountName -or -not $ContainerName) {
-    Write-Error "ResourceGroupName, StorageAccountName and ContainerName must be specified."
+    Write-Error 'ResourceGroupName, StorageAccountName and ContainerName must be specified.'
     return
 }
 
@@ -76,12 +88,12 @@ if (-not $ResourceGroupName -or -not $StorageAccountName -or -not $ContainerName
 try {
     $result = Get-AzContext -ErrorAction Stop
     if (-not $result.Environment) {
-        Write-Error "Please login (Connect-AzAccount) and set the proper subscription (Select-AzSubscription) context before proceeding."
+        Write-Error 'Please login (Connect-AzAccount) and set the proper subscription (Select-AzSubscription) context before proceeding.'
         return
     }
 }
 catch {
-    Write-Error "Please login (Connect-AzAccount) and set the proper subscription (Select-AzSubscription) context before proceeding."
+    Write-Error 'Please login (Connect-AzAccount) and set the proper subscription (Set-AzContext) context before proceeding.'
     return
 }
 
@@ -91,31 +103,77 @@ if (-not $storageAccount) {
     return
 }
 
+# validate subscriptions
+if ($AllSubscriptions -and $Subscription) {
+    Write-Error '-AllSubscriptions and -Subscription can not be used together'
+    return
+}
+
+$savedContext = Get-AzContext
+$validSubscriptions = Get-AzSubscription
+
+if ($AllSubscriptions) {
+    $subscriptions = $validSubscriptions
+
+} elseif (-not $Subscription) {
+    $subscriptions = $validSubscriptions | Where-Object {$_.SubscriptionId -eq $savedContext.Subscription}
+
+} else {
+    $subscriptions = @()
+    foreach ($testSubscription in $Subscription) {
+        if ([System.Guid]::TryParse($testSubscription, [System.Management.Automation.PSReference] [System.Guid]::empty)) {
+            $found = $validSubscriptions | Where-Object {$_.Id -eq $testSubscription}
+        } else {
+            $found = $validSubscriptions | Where-Object {$_.Name -eq $testSubscription}
+        }
+
+        if (-not $found) {
+            Write-Error "Unable to find subscription ($testSubscription)"
+            return
+        }
+        $subscriptions += $found
+    }
+}
+
+# work through each day at a time
 $fileCount = 0
 $workDate = Get-Date -Date $StartDate.ToString('yyyy-MM-dd')
 
 while ($workDate -le $EndDate) {
-
     try {
         $filename = "UsageDetails-$($workDate.ToString('yyyy-MM-dd')).csv"
         $path = Join-Path -Path "$([System.IO.Path]::GetTempPath())" -ChildPath $filename
+        $addingToFile = $false
 
-        Write-Progress -Activity "Usage Details" -Status "Generating $filename"
-        $workEndTime = $workDate.AddHours(23).AddMinutes(59).AddSeconds(59)
+        foreach ($currentSubscription in $subscriptions) {
+            $results = $currentSubscription | Set-AzContext
 
-        Get-AzConsumptionUsageDetail -StartDate $workDate -EndDate $workEndTime -Expand 'MeterDetails' -ErrorAction Stop
-            | Select-Object *,@{Name='Tags'; Expression={$_.Tags ? (ConvertTo-Json $_.Tags) : ''}} -ExpandProperty MeterDetails -ExcludeProperty MeterDetails, Tags
-            | Export-Csv -Path $path
+            Write-Progress -Activity "Working on $filename" -Status "Getting usage for $($currentSubscription.Name)"
+            $workEndTime = $workDate.AddHours(23).AddMinutes(59).AddSeconds(59)
 
+            $usage = Get-AzConsumptionUsageDetail -StartDate $workDate -EndDate $workEndTime -IncludeMeterDetails -Expand 'AdditionalProperties' -ErrorAction Stop `
+            | Select-Object -ExpandProperty MeterDetails -ExcludeProperty Name,MeterDetails, Tags `
+            -Property *, `
+            @{Name='Tags'; Expression = { $_.Tags ? (ConvertTo-Json $_.Tags) : '' } }, `
+            @{Name='ResourceGroupName'; Expression = { $_.InstanceId.Split('/')[4] } }
+            | ConvertTo-Csv -NoTypeInformation -NoHeader:$addingToFile
+            | Out-File -FilePath $path -Append:$addingToFile
+
+            $addingToFile = $true
+        }
         # upload to storage container
-        Write-Progress -Activity "Usage Details" -Status "Uploading blob $filename"
+        Write-Progress -Activity "Working on $filename" -Status "Uploading blob to $StorageAccountName/$ContainerName"
         $results = Set-AzStorageBlobContent -File $path -Container $ContainerName -Blob $filename -Context $storageAccount.Context -Force
         Write-Host "$filename uploaded to $StorageAccountName/$ContainerName"
 
         $fileCount++
-    } catch {
-        Write-Error "Unable to creating & upload $filename to $StorageAccountName/$ContainerName"
-        throw $_.Exception
+    }
+    catch {
+        Write-Error "Unable to get data for $($currentSubscription.Name)"
+        throw $_
+
+    } finally {
+        $result = $savedContext | Set-AzContext
     }
 
     # clean up
@@ -124,4 +182,5 @@ while ($workDate -le $EndDate) {
     $workDate = $workDate.AddDays(1)
 }
 
+$result = $savedContext | Set-AzContext
 Write-Host "$fileCount files uploaded"
