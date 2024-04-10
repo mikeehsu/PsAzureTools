b0f3400b3391a0b61c8ed7558860ab455a286cb0
Mike Hsu <mikeehsu@gmail.com>
2024-04-10 10:09:32 -0400

    added -ShowDetails switch

--- Set-TlsMinVersion.ps1
+++ Set-TlsMinVersion.ps1
@@ -1,39 +1,79 @@
+<#
+.SYNOPSIS
+Updates storage accounts and web apps to use TLS 1.2.
+
+.DESCRIPTION
+This script checks the existing storage accounts and web apps for their TLS versions and updates them to use TLS 1.2 if needed.
+
+.PARAMETER ShowDetails
+A switch parameter to display the TLS versions of all storage accounts and web apps.
+
+.PARAMETER ApplyTLS12
+A switch parameter to apply the update to TLS 1.2. If not supplied, the script will only display the current TLS versions. No updates will be made.
+
+.EXAMPLE
+.\Set-TlsMinVersion.ps1 -ShowDetails
+This command will display the TLS versions of all storage accounts and web apps .
+
+.EXAMPLE
+.\Set-TlsMinVersion.ps1 -ApplyTLS12
+This command will update all storage accounts and web apps to use TLS 1.2.
+#>
+
 [CmdletBinding()]
 
 param (
     [Parameter()]
+    [switch] $ShowDetails,
+
+    [Parameter()]
     [switch] $ApplyTls12
 )
-
-Set-StrictMode -Version 2
-
-$ErrorActionPreference = 'Stop'
-
 
 function Update-StorageAccounts {
     [CmdletBinding()]
 
     param (
         [Parameter()]
+        [switch] $ShowDetails,
+
+        [Parameter()]
         [switch] $ApplyTLS12
     )
 
     Write-Host '===== Checking Storage Accounts ====='
-    $webApps = Get-AzStorageAccount
-    Write-Host "$($webApps.Count) storage accounts"
+    $storageAccounts = Get-AzStorageAccount
+    if ($storageAccounts) {
+        Write-Host "$($storageAccounts.Count) storage accounts in subscription"
+    }
+    else {
+        Write-Host 'No storage accounts found in subscription.'
+    }
 
-    $tlsGroups = $webApps | Group-Object -Property MinimumTLSVersion
+    $tlsGroups = $storageAccounts | Group-Object -Property MinimumTLSVersion
     foreach ($tlsGroup in $tlsGroups) {
+        Write-Host ''
         Write-Host "$($tlsGroup.Name) - $($tlsGroup.Count) storage accounts"
+
+        if (-not $ShowDetails) {
+            Continue
+        }
+
+        # list name of each storage account
+        foreach ($acount in $tlsGroup.Group) {
+            Write-Host "$($acount.StorageAccountName)"
+        }
     }
 
     # select all accounts without TLS 1.2
     $tls12Group = $tlsGroups | Where-Object { $_.Name -ne 'TLS1_2' }
     if (-not $tls12Group) {
-        Write-Host "No storage accounts updates needed."
+        Write-Host ''
+        Write-Host 'No storage accounts updates needed.'
         return
     }
 
+    Write-Host ''
     Write-Host "$($tls12Group.Count) storage accounts need updating to TLS 1.2"
 
     # Apply TLS 1.2
@@ -63,12 +103,20 @@ function Update-WebApp {
 
     param (
         [Parameter()]
+        [switch] $ShowDetails,
+
+        [Parameter()]
         [switch] $ApplyTLS12
     )
 
     Write-Host '===== Checking WebApp ====='
     $webApps = Get-AzWebApp
-    Write-Host "$($webApps.Count) web apps"
+    if ($webApps) {
+        Write-Host "$($webApps.Count) web apps in subscription"
+    }
+    else {
+        Write-Host 'No web apps found.'
+    }
 
     # when getting all webapps without -ResourceGroupName and -Name parameters
     # the MinTlsVersion setting is not populated. We need to step through each
@@ -77,9 +125,9 @@ function Update-WebApp {
     foreach ($webapp in $webApps) {
         $webAppDetail = Get-AzWebApp -ResourceGroupName $webapp.ResourceGroup -Name $webapp.Name
         $webAppDetails += [PSCustomObject] @{
-            Name = $webAppDetail.Name
+            Name              = $webAppDetail.Name
             ResourceGroupName = $webAppDetail.ResourceGroup
-            MinTlsVersion = $webAppDetail.SiteConfig.MinTlsVersion
+            MinTlsVersion     = $webAppDetail.SiteConfig.MinTlsVersion
         }
     }
 
@@ -89,16 +137,28 @@ function Update-WebApp {
         if (-not $name) {
             $name = '<default>'
         }
+        Write-Host ''
         Write-Host "$($name) - $($tlsGroup.Count) web apps"
+
+        if (-not $ShowDetails) {
+            Continue
+        }
+
+        # list name of each app
+        foreach ($app in $tlsGroup.Group) {
+            Write-Host "$($app.Name)"
+        }
     }
 
     # select all accounts without TLS 1.2
     $tls12Group = $tlsGroups | Where-Object { $_.Name -ne '1.2' }
     if (-not $tls12Group) {
-        Write-Host "No webapp updates needed."
+        Write-Host ''
+        Write-Host 'No webapp updates needed.'
         return
     }
 
+    Write-Host ''
     Write-Host "$($tls12Group.Count) web apps need updating to TLS 1.2"
 
     # Apply TLS 1.2
@@ -120,5 +180,27 @@ function Update-WebApp {
 
 }
 
-Update-StorageAccounts -ApplyTLS12:$ApplyTLS12
-Update-WebApp -ApplyTLS12:$ApplyTLS12
+###########
+## MAIN
+###########
+
+Set-StrictMode -Version 2
+
+$ErrorActionPreference = 'Stop'
+
+# confirm user is logged into subscription
+try {
+    $result = Get-AzContext -ErrorAction Stop
+    if (-not $result.Environment) {
+        Write-Error 'Please login (Connect-AzAccount) and set the proper subscription (Select-AzSubscription) context before proceeding.'
+        return
+    }
+
+}
+catch {
+    Write-Error 'Please login (Connect-AzAccount) and set the proper subscription (Select-AzSubscription) context before proceeding.'
+    return
+}
+
+Update-StorageAccounts -ShowDetails:$ShowDetails -ApplyTLS12:$ApplyTLS12 -ErrorAction Stop
+Update-WebApp -ShowDetails:$ShowDetails  -ApplyTLS12:$ApplyTLS12 -ErrorAction Stop
